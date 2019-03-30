ae4f0924e67a891916898aa98ac7ca7f2745ef49
techhtml <apes0123@gmail.com>
2019-03-30 01:14:22 +0000

    feat(-): remove unused component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
-import { AttendingComponent } from './attending/attending.component';
 import { ScheduleComponent } from './schedule/schedule.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { TeamComponent } from './team/team.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent },
-  { path: 'schedule', component: ScheduleComponent },
-  { path: 'attending', component: AttendingComponent },
-  { path: 'registration', component: RegistrationComponent },
-  { path: 'team', component: TeamComponent }
+  { path: 'schedule', component: ScheduleComponent }
 ]
 
 @NgModule({
